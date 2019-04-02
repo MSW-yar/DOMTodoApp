bac64d54a20345fbc99086828aba3e417e400e21
Usairim Isani <usairim.isani@outlook.com>
2019-04-02 21:16:08 +0500

    Now Takes input on Enter.

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import InputEl from "./InputEl";
 import HtmlEl from "./HtmlEl";
 (function() {
+  function liAttacher(e) {
+    if (
+      e.type == "click" ||
+      (e.type == "keyup" && e.key == "Enter" )
+    ) {
+      let li = new HtmlEl("li", ul.getNode());
+      let inpCheck = new InputEl(li.getNode(), "checkbox");
+      inpCheck.attachHandler("onchange", e => {
+        inpCheck.getNode().parentNode.style.textDecoration = e.target.checked
+          ? "line-through"
+          : "none";
+      });
+      li.insertText(`  ${input.getValue()}  `);
+      let delBut = new HtmlEl("button", li.getNode());
+      delBut.insertText("Delete");
+      delBut.attachHandler("onclick", e => {
+        let tr = e.target;
+        delBut.removeChild(tr.parentNode, tr.parentNode.parentNode);
+      });
+    }
+  }
   let app = document.querySelector("#app");
   let input = new InputEl(app);
+  input.attachHandler("onkeyup",liAttacher);
   let button = new HtmlEl("button", app);
   button.insertText("Click Me");
   let ul = new HtmlEl("ul", app);
-  button.attachHandler("onclick", e => {
-    let li = new HtmlEl("li", ul.getNode());
-    let inpCheck = new InputEl(li.getNode(), "checkbox");
-    inpCheck.attachHandler("onchange", e => {
-      let deco = inpCheck.getNode().parentNode.style;
-      e.target.checked ? (deco.textDecoration = "line-through") : (deco.textDecoration = "none");
-    });
-    li.insertText(`  ${input.getValue()}  `);
-    let delBut = new HtmlEl("button", li.getNode());
-    delBut.insertText("Delete");
-    delBut.attachHandler("onclick", e => {
-      let tr = e.target;
-      delBut.removeChild(tr.parentNode, tr.parentNode.parentNode);
-    });
-  });
+  button.attachHandler("onclick", liAttacher);
 })();
